e29e5ac6ca7026579f5cf29ac070530652e5a22c
Kamrul Hasan <116941825+kamrul35728@users.noreply.github.com>
2024-02-04 23:07:34 +0600

    update count
    How many edges will be in spanning tree update their count

--- minimum_spanning_tree.cpp
+++ minimum_spanning_tree.cpp
@@ -1,83 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-bool dfs(int node, vector<vector<int>> &adj, vector<bool> &visited, int parent)
-{
+bool dfs(int node, vector<vector<int>> &adj, vector<bool> &visited, int parent){
     visited[node] = true;
-    for (auto &it : adj[node])
-    {
-        if (!visited[it])
-        {
-            if (dfs(it, adj, visited, node))
-            {
+    for (auto &it : adj[node]){
+        if (!visited[it]){
+            if (dfs(it, adj, visited, node)){
                 return true;
             }
         }
-        else if (it != parent)
-        {
+        else if (it != parent){
             return true;
         }
     }
     return false;
 }
-
-bool hasCycle(int V, vector<vector<int>> &adj)
-{
+bool hasCycle(int V, vector<vector<int>> &adj){
     vector<bool> visited(V + 1, false);
     return dfs(1, adj, visited, -1);
 }
-
-int minimum_spanning_tree(int V, multimap<int, pair<int, int>> &mp, vector<vector<int>> &adj)
-{
+int minimum_spanning_tree_cost(int V, multimap<int, pair<int, int>> &mp, vector<vector<int>> &adj){
     int minWeight = 0;
     int cnt = 0;
-    for (auto it = mp.begin(); it != mp.end(); it++)
-    {
-        cnt++;
+    for (auto it = mp.begin(); it != mp.end(); it++){
         // Temporarily add the edge
         int node = it->second.first;
         int adjNode = it->second.second;
         int wg = it->first;
         adj[node].push_back(adjNode);
         adj[adjNode].push_back(node);
-
-        // Check if adding the edge creates a cycle
-        if (!hasCycle(V, adj))
-        {
+        if (!hasCycle(V, adj)){
             minWeight += wg;
+            cnt++;
         }
-        else
-        {
-            // Remove the edge
+        else{
             adj[node].pop_back();
             adj[adjNode].pop_back();
         }
-
-        if (cnt == V - 1)
-        {
+        if (cnt == V - 1){
             break;
         }
     }
     return minWeight;
 }
-
-int main()
-{
+int main(){
     int V, E;
     cin >> V >> E;
-    vector<vector<int>> adj(V + 1); // Adjacency list
-
+    vector<vector<int>> adj(V + 1);
     multimap<int, pair<int, int>> mp;
-    for (int i = 1; i <= E; i++)
-    {
+    for (int i = 1; i <= E; i++){
         int u, v, w;
         cin >> u >> v >> w;
         mp.insert({w, {u, v}});
     }
-
-    int weight = minimum_spanning_tree(V, mp, adj);
+    int weight = minimum_spanning_tree_cost(V, mp, adj);
     cout << "Minimum Spanning Tree is: " << weight << endl;
-
     return 0;
 }
 // 5 8
@@ -89,3 +65,14 @@ int main()
 // 2 4 2
 // 1 5 3
 // 3 4 4
+
+
+// 5 8
+// 1 2 2
+// 2 3 5
+// 3 5 1
+// 4 5 2
+// 1 4 5
+// 2 4 3
+// 1 5 4
+// 2 5 3
